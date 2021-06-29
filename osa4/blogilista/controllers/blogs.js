8ea4f581e8c5d47f7dd5ced27f2fde4cc5c4f7f9
Otto Takkinen <otto.takkinen@gmail.com>
2021-06-30 00:18:46 +0300

    Add backend tests for blogilista (4.8-4.14)

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,18 +2,39 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const config = require('../utils/config');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
+  if (request.body.title === undefined || request.body.url === undefined) {
+    return response.status(400).end();
+  }
   const blog = new Blog(request.body);
+  const result = await blog.save();
+  response.status(201).json(result);
+});
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
   });
+  response.json(updatedBlog);
 });
 
 // blogsRouter.listen(config.PORT, () => {
---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blog');
+const initialBlogs = [
+  {
+    title: 'test blog 1',
+    author: 'Otto T. Akkinen',
+    url: 'test.fi',
+    likes: 5,
+  },
+  {
+    title: 'test blog 2',
+    author: 'Otto K. Akkinen',
+    url: 'test.fi/2',
+    likes: 1,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('blog identifier is named id', async () => {
+  const res = await api.get('/api/blogs');
+
+  expect(res.body[0].id).toBeDefined();
+});
+
+test('blogs.length is one more than initial after adding a blog', async () => {
+  const newBlog = {
+    author: 'nönnöönöö',
+    title: 'test',
+    url: 'test',
+    likes: 0,
+  };
+
+  await api.post('/api/blogs').send(newBlog);
+
+  const res = await api.get('/api/blogs');
+
+  expect(res.body.length).toBe(initialBlogs.length + 1);
+});
+
+test('likes defaults to 0', async () => {
+  const newBlog = {
+    author: 'nönnöönöö',
+    title: 'test',
+    url: 'test',
+  };
+  await api.post('/api/blogs').send(newBlog);
+
+  const res = await api.get('/api/blogs');
+
+  const newestBlog = res.body[res.body.length - 1];
+
+  expect(newestBlog.likes).toBe(0);
+});
+
+test('post without title returns 400 Bad Request', async () => {
+  const newBlog = {
+    author: 'nönnöönöö',
+    url: 'test',
+    likes: 0,
+  };
+  await api.post('/api/blogs').send(newBlog).expect(400);
+});
+
+test('post without url returns 400 Bad Request', async () => {
+  const newBlog = {
+    author: 'nönnöönöö',
+    title: 'test',
+    likes: 0,
+  };
+  await api.post('/api/blogs').send(newBlog).expect(400);
+});
+
+test('deleting by id works', async () => {
+  const getResponse = await api.get('/api/blogs');
+  const blogsBeforeDeleting = getResponse.body;
+  await api.delete(`/api/blogs/${blogsBeforeDeleting[0].id}`);
+  const afterResponse = await api.get('/api/blogs');
+  expect(afterResponse.body.length).toBe(blogsBeforeDeleting.length - 1);
+});
+
+test('updating likes works', async () => {
+  const response = await api.get('/api/blogs');
+  const newBlog = { ...response.body[0], likes: 420 };
+
+  console.log(newBlog);
+  const res = await api.put(`/api/blogs/${newBlog.id}`).send(newBlog);
+  console.log('updated blog: ', res.body);
+
+  const response2 = await api.get('/api/blogs');
+
+  console.log(response2.body);
+  expect(response2.body[0].likes).toBe(newBlog.likes);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
